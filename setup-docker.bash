e207a43a2c3209339affbd8db81dfa965f40a1f8
Gorag <greg6821@gmail.com>
2026-01-09 07:01:43 +0300

    Update setup-docker.bash

--- setup-docker.bash
+++ setup-docker.bash
@@ -8,14 +8,7 @@ docker run -d \
   devkitpro/devkita64:20240827 \
   tail -f /dev/null
 
-# ---- FIX DEBIAN BUSTER EOL ----
-docker exec switchdev bash -c '
-  sed -i "s|deb.debian.org|archive.debian.org|g" /etc/apt/sources.list &&
-  sed -i "s|security.debian.org|archive.debian.org|g" /etc/apt/sources.list &&
-  echo "Acquire::Check-Valid-Until false;" > /etc/apt/apt.conf.d/99no-check-valid-until
-'
-
-# update after fixing repos
+# update 
 docker exec switchdev apt-get update
 
 # base deps
