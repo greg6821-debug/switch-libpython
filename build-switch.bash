1c73158cc6e62dafa826d6f13d3a7933a3819a22
Gorag <greg6821@gmail.com>
2026-01-09 06:16:42 +0300

    Update build-switch.bash

--- build-switch.bash
+++ build-switch.bash
@@ -1,7 +1,6 @@
 set -e
 
 export DEVKITPRO=/opt/devkitpro
-export PYTHON_FOR_BUILD=/usr/bin/python3.9
 
 source $DEVKITPRO/switchvars.sh
 pushd Python-3.9.22
@@ -10,7 +9,7 @@ cp ../cpython_config_files/config.site build-switch
 pushd build-switch
 mkdir local_prefix
 export LOCAL_PREFIX=$(realpath local_prefix)
-$PYTHON_FOR_BUILD ../configure \
+../configure \
   --host=aarch64-none-elf \
   --build=$(../config.guess) \
   --prefix="$LOCAL_PREFIX" \
