1ee80181514bbfb02ac60eacec9ed348b20ab733
Gorag <greg6821@gmail.com>
2026-01-09 05:01:38 +0300

    Update build-switch.bash

--- build-switch.bash
+++ build-switch.bash
@@ -1,48 +1,61 @@
+#!/usr/bin/env bash
 set -e
 
+# --- Настройки ---
 export PREFIXARCHIVE=$(realpath python39-switch.tar.gz)
 export DEVKITPRO=/opt/devkitpro
 
 source $DEVKITPRO/switchvars.sh
 
+# --- Переходим в исходники Python ---
 pushd Python-3.9.22
-#pushd cpython
-mkdir build-switch
+
+mkdir -p build-switch
 cp ../cpython_config_files/config.site build-switch
 pushd build-switch
-mkdir local_prefix
+
+mkdir -p local_prefix
 export LOCAL_PREFIX=$(realpath local_prefix)
+
+# --- Конфигурация cross-build ---
 PYTHON_FOR_BUILD=python3 \
+LDFLAGS="-specs=$DEVKITPRO/libnx/libnx.specs $LDFLAGS" \
+CONFIG_SITE="config.site" \
 ../configure \
   --host=aarch64-none-elf \
-  --build=$(../config.guess) \
+  --build="$(../config.guess)" \
   --prefix="$LOCAL_PREFIX" \
   --disable-ipv6 \
   --disable-shared \
   --without-pymalloc \
-  LDFLAGS="-specs=$DEVKITPRO/libnx/switch.specs $LDFLAGS" \
-  CONFIG_SITE="config.site"
-popd
-cp ../cpython_config_files/Setup.local build-switch/Modules
-pushd build-switch
+  --enable-optimizations
+
+# --- Копируем локальный Setup для сборки модулей ---
+cp ../cpython_config_files/Setup.local Modules
+
+# --- Сборка только статической библиотеки ---
 make -j $(getconf _NPROCESSORS_ONLN) libpython3.9.a
+
+# --- Ручная установка статической библиотеки и заголовков ---
 mkdir -p $LOCAL_PREFIX/lib
 cp libpython3.9.a $LOCAL_PREFIX/lib/libpython3.9.a
-make libinstall
-make inclinstall
+
+mkdir -p $LOCAL_PREFIX/include
+cp -r ../Include/* $LOCAL_PREFIX/include/
+
 popd
 popd
 
+# --- Пакуем результат в артефакт ---
 mkdir -p ./python39-switch
-
 mv $LOCAL_PREFIX/* ./python39-switch/
 
+# --- Минимизация библиотеки ---
 pushd python39-switch/lib/python3.9
-rm -r test
-rm -r lib2to3/tests
+rm -rf test lib2to3/tests
 rm subprocess.py
 cp ../../../stub/subprocess.py ./
-find . -type l -not -name \*.py -delete
+find . -type l -not -name "*.py" -delete
 find . -type d -empty -delete
-find . -name \*.py -exec python3 -OO -m py_compile {} \;
+find . -name "*.py" -exec python3 -OO -m py_compile {} \;
 popd
