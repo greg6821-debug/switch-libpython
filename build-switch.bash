101ebf938a57c640c912c12b0ec8f0c9ad034af4
Gorag <greg6821@gmail.com>
2026-01-09 04:36:16 +0300

    Update build-switch.bash

--- build-switch.bash
+++ build-switch.bash
@@ -1,27 +1,24 @@
-
 set -e
 
-export PREFIXARCHIVE=$(realpath python39-switch.tar.gz)
+export DEVKITPRO=/opt/devkitpro
 
 source $DEVKITPRO/switchvars.sh
-pushd Python-3.9.22
+pushd cpython
 mkdir build-switch
 cp ../cpython_config_files/config.site build-switch
 pushd build-switch
 mkdir local_prefix
 export LOCAL_PREFIX=$(realpath local_prefix)
-PYTHON_FOR_BUILD=python3 \
-LDFLAGS="-specs=$DEVKITPRO/libnx/switch.specs $LDFLAGS" \
-CONFIG_SITE="config.site" \
 ../configure \
   --host=aarch64-none-elf \
-  --build="$(../config.guess)" \
+  --build=$(../config.guess) \
   --prefix="$LOCAL_PREFIX" \
   --disable-ipv6 \
   --disable-shared \
-  --enable-optimizations
+  --without-pymalloc \
+  LDFLAGS="-specs=$DEVKITPRO/libnx/switch.specs $LDFLAGS" \
+  CONFIG_SITE="config.site"
 popd
-
 cp ../cpython_config_files/Setup.local build-switch/Modules
 pushd build-switch
 make -j $(getconf _NPROCESSORS_ONLN) libpython3.9.a
@@ -32,4 +29,16 @@ make inclinstall
 popd
 popd
 
-tar -czvf $PREFIXARCHIVE -C $LOCAL_PREFIX .
+mkdir -p ./python39-switch
+
+mv $LOCAL_PREFIX/* ./python39-switch/
+
+pushd python39-switch/lib/python3.9
+rm -r test
+rm -r lib2to3/tests
+rm subprocess.py
+cp ../../../stub/subprocess.py ./
+find . -type l -not -name \*.py -delete
+find . -type d -empty -delete
+find . -name \*.py -exec python3 -OO -m py_compile {} \;
+popd
