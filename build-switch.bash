b14e3cd3e6838ce441b83da38ca85c3523e883a6
Gorag <greg6821@gmail.com>
2026-01-09 05:17:42 +0300

    Update build-switch.bash

--- build-switch.bash
+++ build-switch.bash
@@ -1,92 +1,44 @@
-#!/usr/bin/env bash
 set -e
 
-# ---------------------------
-# Настройки
-# ---------------------------
-export PREFIXARCHIVE=$(realpath python39-switch.tar.gz)
 export DEVKITPRO=/opt/devkitpro
 
-# Определяем корень проекта (независимо от текущей директории)
-export PROJECT_ROOT=$(realpath "$(dirname "$0")/..")
-
-# Инициализация devkitPro и Switch портов
 source $DEVKITPRO/switchvars.sh
-
-# ---------------------------
-# Переходим в исходники Python
-# ---------------------------
 pushd Python-3.9.22
-
-mkdir -p build-switch
+mkdir build-switch
+cp ../cpython_config_files/config.site build-switch
 pushd build-switch
-
-# Копируем необходимые конфиги
-cp "$PROJECT_ROOT/cpython_config_files/config.site" .
-mkdir -p Modules
-cp "$PROJECT_ROOT/cpython_config_files/Setup.local" Modules/
-
-mkdir -p local_prefix
+mkdir local_prefix
 export LOCAL_PREFIX=$(realpath local_prefix)
-
-# ---------------------------
-# Настройка toolchain
-# ---------------------------
-export CC=aarch64-none-elf-gcc
-export CXX=aarch64-none-elf-g++
-export LD=aarch64-none-elf-ld
-export AR=aarch64-none-elf-ar
-export RANLIB=aarch64-none-elf-ranlib
-export SPEC="$DEVKITPRO/libnx/switch.specs"
-
-export LDFLAGS="-specs=$SPEC -L$DEVKITPRO/portlibs/switch/lib $LDFLAGS"
-export CPPFLAGS="-I$DEVKITPRO/portlibs/switch/include $CPPFLAGS"
-export CFLAGS="-O2 -march=armv8-a+crc+crypto -mtune=cortex-a57 -fPIC -fPIE $CFLAGS"
-
-# ---------------------------
-# Конфигурация cross-build
-# ---------------------------
-PYTHON_FOR_BUILD=python3 \
-CONFIG_SITE=config.site \
 ../configure \
   --host=aarch64-none-elf \
-  --build="$(../config.guess)" \
+  --build=$(../config.guess) \
   --prefix="$LOCAL_PREFIX" \
   --disable-ipv6 \
   --disable-shared \
   --without-pymalloc \
-  --enable-optimizations \
-  LDFLAGS="$LDFLAGS" \
-  CPPFLAGS="$CPPFLAGS" \
-  CFLAGS="$CFLAGS"
-
-# ---------------------------
-# Сборка статической библиотеки
-# ---------------------------
+  LDFLAGS="-specs=$DEVKITPRO/libnx/switch.specs $LDFLAGS" \
+  CONFIG_SITE="config.site"
+popd
+cp ../cpython_config_files/Setup.local build-switch/Modules
+pushd build-switch
 make -j $(getconf _NPROCESSORS_ONLN) libpython3.9.a
-
-# Установка вручную
 mkdir -p $LOCAL_PREFIX/lib
 cp libpython3.9.a $LOCAL_PREFIX/lib/libpython3.9.a
-
-mkdir -p $LOCAL_PREFIX/include
-cp -r ../Include/* $LOCAL_PREFIX/include/
-
+make libinstall
+make inclinstall
 popd
 popd
 
-# ---------------------------
-# Пакуем артефакт
-# ---------------------------
 mkdir -p ./python39-switch
+
 mv $LOCAL_PREFIX/* ./python39-switch/
 
-# Минимизация Python-stdlib
 pushd python39-switch/lib/python3.9
-rm -rf test lib2to3/tests
+rm -r test
+rm -r lib2to3/tests
 rm subprocess.py
 cp ../../../stub/subprocess.py ./
-find . -type l -not -name "*.py" -delete
+find . -type l -not -name \*.py -delete
 find . -type d -empty -delete
-find . -name "*.py" -exec python3 -OO -m py_compile {} \;
+find . -name \*.py -exec python3 -OO -m py_compile {} \;
 popd
