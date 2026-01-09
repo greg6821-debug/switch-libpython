e1793c908bd8cea588598117977bf8c1a0df0f3f
Gorag <greg6821@gmail.com>
2026-01-09 06:49:00 +0300

    Update setup-docker.bash

--- setup-docker.bash
+++ setup-docker.bash
@@ -1,5 +1,5 @@
 
-docker run -d --name switchdev --workdir /build/git -v "${PWD}:/build/git" devkitpro/devkita64:20240409 tail -f /dev/null
+docker run -d --name switchdev --workdir /build/git -v "${PWD}:/build/git" devkitpro/devkita64:20210514 tail -f /dev/null
 curl -LOC - https://github.com/uyjulian/pacman-packages/releases/download/v2.2.3-1-pkgbuild-helpers/devkitpro-pkgbuild-helpers-2.2.3-1-any.pkg.tar.xz
 docker exec switchdev /bin/bash -c 'dkp-pacman -U --noconfirm devkitpro-pkgbuild-helpers-2.2.3-1-any.pkg.tar.xz'
 docker exec switchdev /bin/bash -c 'apt-get -y update'
