ba90812a4a121c55730d7d6798bbbdcdbcd32ec4
Gorag <greg6821@gmail.com>
2026-01-20 00:51:48 +0300

    Update build-switch.bash

--- build-switch.bash
+++ build-switch.bash
@@ -1,7 +1,7 @@
 set -e
 
 source $DEVKITPRO/switchvars.sh
-mkdir Python-3.9.22/Modules
+
 # Копируем наши видео-плеер файлы в Python
 echo "Copying video player files to Python build..."
 cp -r video_player Python-3.9.22/Modules/
